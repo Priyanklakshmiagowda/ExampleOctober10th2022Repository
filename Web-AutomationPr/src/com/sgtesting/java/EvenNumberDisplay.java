29eac928b39d480b4a8f11a266b619eda15c2b1f
Priyankalakshmigowda <priyankaljdp@gmail.com>
2023-01-02 17:37:32 +0530

    EvenNumberDisply

---
+++ Web-AutomationPr/src/com/sgtesting/java/EvenNumberDisplay.java
@@ -0,0 +1,15 @@
+package com.sgtesting.java;
+
+public class EvenNumberDisplay {
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<20;i++)
+		{
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
